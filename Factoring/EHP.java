b7f0c3dbc8cd3a205713faf5fa74e314fb94314d
karltrowbridge <kttrowbridge@stu.naperville203.org>
2018-10-19 15:57:59 -0500

    did some shizzle off the hizlle

---
+++ Factoring/EHP.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import javax.swing.JFrame;
+/**
+ * issa vpn.
+ *
+ * @author karltrowbridge
+ * @version 9/21/18
+ */
+public class EHP
+{
+    private int numOfClasses;
+    
+    public EHP(int classesNum)
+    {
+        this.numOfClasses = classesNum;
+        for(int x=1;x<=this.numOfClasses; x++)
+        {
+            
+        }
+    }
+}
