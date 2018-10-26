0c53787853fd4a8fbb701032e60600b4eac32dce
karltrowbridge <kttrowbridge@stu.naperville203.org>
2018-10-26 15:58:58 -0500

    Got the list working, working on adding stuff

--- Factoring/EHP.java
+++ Factoring/EHP.java
@@ -1,3 +1,5 @@
+
+import java.util.ArrayList;
 import java.util.Scanner;
 import javax.swing.JFrame;
 /**
@@ -9,13 +11,40 @@ import javax.swing.JFrame;
 public class EHP
 {
     private int numOfClasses;
-    
-    public EHP(int classesNum)
+    ArrayList classList = new ArrayList();
+    public EHP()
     {
-        this.numOfClasses = classesNum;
+        // number of classes
+        Scanner s = new Scanner(System.in);
+        System.out.println("How many classes do you have: ");
+        this.numOfClasses = s.nextInt();
+        
+        // class names
+        
+        String className;
+        System.out.println("Input class name: ");
         for(int x=1;x<=this.numOfClasses; x++)
         {
             
+            className = s.nextLine();
+            classList.add(className);
         }
+        
+        System.out.println(classList);
+    }
+    
+    public void addClass()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("What class do you want to add?: ");
+        String className = s.nextLine();
+        classList.add(className);
+    }
+    
+    public void addHomework()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("What class?: ");
+        //String className = s.nextln()
     }
 }
